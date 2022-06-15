bf89f7d43bfabd14a88cc90fe7b96210400e322b
Sheikh Yeamin <49518797+SheikhULAB@users.noreply.github.com>
2022-06-16 01:21:37 +0600

    solidity default value types

---
+++ DefaultValues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DefaultValues {
+    bool public b;
+    uint public u;
+    int public i;
+    address public a;
+    bytes32 public b32;
+    
+}
